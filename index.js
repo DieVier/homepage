801aa2bb152e0976f36635bc23c9737356f35e0b
justayak <jutanke@googlemail.com>
2015-08-16 23:44:04 +0200

    page stuff

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var express = require('express');
 var PORT = 8080;
 
 var app = express();
+
+app.use(express.static(__dirname + "/static"));
+
 app.get('/', function(req, res) {
-    res.send('Hallo Welt\n');
+    res.send(__dirname + "/index.html");
 });
 
 app.listen(PORT);
